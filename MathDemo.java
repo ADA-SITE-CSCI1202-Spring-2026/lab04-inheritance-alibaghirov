5d65e62a693af56944078436f90fdd7240b8b094
Ali Baghirov <abaghirov20305@ada.edu.az>
2026-02-26 09:45:31 +0400

    added all

---
+++ MathDemo.java
@@ -0,0 +1,32 @@
+public class MathDemo {
+    public static int min(int a, int b ) {
+        if ( a < b ) {
+            return a;
+        }
+        return b;
+    }
+
+    public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        return b;
+    }
+    public static int sum(int[] args) {
+        int total = 0;
+        for ( int num : args ) {
+            total += num;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("Min: " + min(a, b));
+        System.out.println("Max: " + max(a, b));
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("Sum: " + sum(numbers));
+    }
+}
